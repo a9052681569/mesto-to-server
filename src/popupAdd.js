eea421e5d1add26820b3f80b58257ce5f0e3e90e
Andrew <a9052681569@gmail.com>
2019-10-23 17:44:46 +0300

    исправлены комментарии

--- src/popupAdd.js
+++ src/popupAdd.js
@@ -1,53 +1,22 @@
 // импортируем переменные и классы, необходимые для работы этому классу
-import {formAddCard, api, popupAdd} from './index.js';
-import {Popup} from './popup.js';
-import {CardList} from './cardList.js';
+import {api} from './api';
+import {Popup} from './popup';
+import {CardList} from './cardList';
 // класс попапа добавления карточки
-export class PopupAdd extends Popup {
+class PopupAdd extends Popup {
     constructor(popup, btn) {
       super(popup, btn);
+      this.api = api;
+      this.add = this.add.bind(this);
       // слушатель клика по кнопке добавить карточку ("+") открывает форму добавления
       this.button.addEventListener('click', () => {
         formAddCard.elements.link.nextElementSibling.textContent = 'Это обязательное поле';
         formAddCard.elements.name.nextElementSibling.textContent = 'Это обязательное поле';
-        popupAdd.toggle();
-        popupAdd.handler();
+        this.toggle();
+        this.handler();
       });
     // слушатель, добавляющий карточку, с введенными в поля ввода названием и картинкой
-      formAddCard.addEventListener('submit', function(event) {
-        // избегаем перезагрузки страници
-          event.preventDefault();
-          document.querySelector('.popup__button').textContent = 'Загрузка...';
-        // добавляем в константы путь к данным из полей ввода
-          const name = formAddCard.elements.name.value;
-          const link = formAddCard.elements.link.value;
-        // добавляем карточку (берем название места и ссылку на картинку из аргументов)
-          api.cardToServer(name, link)
-            // после загрузки на сервер добавляем карточку на страницу
-            .then((res) => {
-              if(res.ok) {
-                document.querySelector('.places-list').innerHTML = '';
-                api.getInitialCards()
-                // этом этапе res - это массив карточек, отрисовываем их
-                .then(res => new CardList(document.querySelector('.places-list'), res))
-                .catch(err => console.log(err));
-              }
-            // после успешной обработки запроса закрываем и сбрасываем форму
-              if(res.status === 200) {
-                popupAdd.toggle();
-                formAddCard.reset();
-              }
-            })
-            // в случае ошибки закрываем и сбрасываем форму, выводим сообщение об ошибке
-            .catch((err) => {
-              popupAdd.toggle();
-              formAddCard.reset();
-              alert(`Ошибка: ${err}`);
-            })
-            .finally((res) => {
-              document.querySelector('.popup__button').textContent = '+';
-            })
-      });
+      formAddCard.addEventListener('submit', this.add);
     // слушатель, валидирующий форму ввода карточки
       formAddCard.addEventListener('input', this.handler);
     }
@@ -82,4 +51,42 @@ export class PopupAdd extends Popup {
         name.nextElementSibling.textContent = '';
       }
     }
+    add(event) {
+      // избегаем перезагрузки страници
+      console.log(event);
+      event.preventDefault();
+      document.querySelector('.popup__button').textContent = 'Загрузка...';
+    // добавляем в константы путь к данным из полей ввода
+      const name = formAddCard.elements.name.value;
+      const link = formAddCard.elements.link.value;
+    // добавляем карточку (берем название места и ссылку на картинку из аргументов)
+      this.api.cardToServer(name, link)
+        // после загрузки на сервер добавляем карточку на страницу
+        .then((res) => {
+          if(res.ok) {
+            document.querySelector('.places-list').innerHTML = '';
+            this.api.getInitialCards()
+            // этом этапе res - это массив карточек, отрисовываем их
+            .then(res => new CardList(document.querySelector('.places-list'), res))
+            .catch(err => console.log(err));
+          }
+        // после успешной обработки запроса закрываем и сбрасываем форму
+          if(res.status === 200) {
+            this.toggle();
+            formAddCard.reset();
+          }
+        })
+        // в случае ошибки закрываем и сбрасываем форму, выводим сообщение об ошибке
+        .catch((err) => {
+          this.toggle();
+          formAddCard.reset();
+          console.log(`Ошибка: ${err}`);
+        })
+        .finally((res) => {
+          document.querySelector('.popup__button').textContent = '+';
+        })
+    }
   }
+
+const { new: formAddCard } = document.forms;
+export const popupAdd = new PopupAdd(document.querySelector('.popup__add'), document.querySelector('.user-info__button_add'));
